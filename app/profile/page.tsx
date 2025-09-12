bb59231b72a61e76a43191dd7a654e706c119ff6
Soham Pande <sohampand747@gmail.com>
2025-09-13 01:40:02 +0530

    dummy profile page added

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ProfilePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Profile</h1>
+      <p>Your profile details go here.</p>
+    </main>
+  );
+}
