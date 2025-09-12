4264bd7b1ac6aec66847d4b8f7f6552a0ed5ea83
Soham Pande <sohampand747@gmail.com>
2025-09-13 01:46:28 +0530

    added default return type to empty settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PageName() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Page Name</h1>
+      <p>Content goes here.</p>
+    </main>
+  );
+}
