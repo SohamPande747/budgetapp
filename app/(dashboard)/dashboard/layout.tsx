e8d764da0f7fd645cf9d83c721c93ce50c4fe470
Soham Pande <sohampande747@gmail.com>
2026-02-16 19:30:50 +0530

    stabble hai to cable hai

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
 import LogoutButton from './LougoutButton'
+import styles from './layout.module.css'
 
 export default async function DashboardLayout({
   children,
@@ -19,28 +20,27 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="layout">
-      {/* Sidebar */}
-      <aside className="sidebar">
-        <h2>Spendle</h2>
+    <div className={styles.layout}>
+      <aside className={styles.sidebar}>
+        <div>
+          <h2 className={styles.logo}>Spendle</h2>
 
-        <nav className="sidebar-nav">
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/dashboard/transactions">Transactions</Link>
-          <Link href="/dashboard/add-transaction">Add Transaction</Link>
-          <Link href="/dashboard/categories">Categories</Link>
-          <Link href="/dashboard/budgets">Budgets</Link>
-          <Link href="/dashboard/accounts">Accounts</Link>
-        </nav>
+          <nav className={styles.sidebarNav}>
+            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/dashboard/transactions">Transactions</Link>
+            <Link href="/dashboard/add-transaction">Add Transaction</Link>
+            <Link href="/dashboard/categories">Categories</Link>
+            <Link href="/dashboard/budgets">Budgets</Link>
+            <Link href="/dashboard/accounts">Accounts</Link>
+          </nav>
+        </div>
 
-        {/* Logout */}
-        <div className="logout-form">
+        <div className={styles.logoutForm}>
           <LogoutButton />
         </div>
       </aside>
 
-      {/* Main Content */}
-      <main className="main">
+      <main className={styles.main}>
         {children}
       </main>
     </div>
